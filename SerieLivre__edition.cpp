383ed9afbd7a8b65a5af5e6e1a118ff7f5f09f46
monsterzo <zouhair.toufani-etu@etu.univh2c.ma>
2023-01-27 17:35:45 +0100

    add some modificaton

--- SerieLivre__edition.cpp
+++ SerieLivre__edition.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
 #include<string.h>
 #include"SerieLivre__edition.h"
-
+using namespace std;
 
 SerieLivre_edition::SerieLivre_edition(char * edit,int nbp): SerieLivre(nbp){
-    for(int  i=0;i<=strelen(edit);i++)
-        edition[i]=edit[i]; 
+    strcpy(edition,edit);
 }
 
-SerieLivre_edition::SerieLivre_edition(const SerieLivre_edition & Se){
-    for(int  i=0;i<=strelen(edit);i++)
-        edition[i]=edit[i];
+SerieLivre_edition::SerieLivre_edition(const SerieLivre_edition & Se): SerieLivre(Se.nbp){
+    strcpy(edition,Se.edition);
+    for(int i=0;i<nbp;i++)
+         T[i]=Se.T[i]; //operateur affectation du premier partie 
+}
+void SerieLivre__edition::Afficher(){
+    cout<<"l'edition est "<<edition<<endl;
+    cout<<"le nbre de livre est "<<nbp<<endl;
+    for(int i=0;i<nbp;i++)
+          T[i].Afficher();
 }
